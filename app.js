3d0bc560e0e6b388620a37e218c4c128f1a11e30
Subodh <you@gmail.com>
2020-05-19 00:12:11 +0530

    next version

--- app.js
+++ app.js
@@ -4,113 +4,106 @@
 
   angular.module("myApp",[])
 
-  .controller("ToBuyController" , ToBuyController)
-  .controller("AlreadyBoughtController" , AlreadyBoughtController)
-  .service("ShoppingListCheckOffService",ShoppingListCheckOffService);
 
+  .controller("NarrowItDownController" , NarrowItDownController )
+  .service("MenuSearchService",MenuSearchService)
+  .directive('foundItems',FoundItemsDirective);
 
-  ToBuyController.$inject = ['ShoppingListCheckOffService'];
-  function ToBuyController(ShoppingListCheckOffService) {
+function FoundItemsDirective() {
+  var ddo = {
+    templateUrl : 'itemsloaderindicator.template.html',
+//    scope:{
+  //    foundItems : '<',
+    //  onRemove: '='
 
-    var itemadd = this;
-    var c = 0 ;
-
-    itemadd.IsVisible1 = 1;
-    itemadd.IsVisible2 = 1;
-    itemadd.IsVisible3 = 1;
-    itemadd.IsVisible4 = 1;
-    itemadd.IsVisible5 = 1;
-    itemadd.IsVisible6 = 0;
-        itemadd.IsVisible7 = 1;
-
-
-    itemadd.name1 = "Cookies";
-    itemadd.quantity1 = 10;
-
-    itemadd.name2 = "Chips";
-    itemadd.quantity2 = 15;
-
-    itemadd.name3 = "IceCream";
-    itemadd.quantity3 = 25;
-
-    itemadd.name4 = "Choco";
-    itemadd.quantity4 = 5;
-
-    itemadd.name5 = "Maggi";
-    itemadd.quantity5 = 10;
-
-
-
-
-    itemadd.addItem = function (a,b) {
-
-        c++
-      ShoppingListCheckOffService.addItem(a,b);
-      switch (a) {
-        case "Cookies":
-                  itemadd.IsVisible1 = 0;
-          break;
-          case "Chips":
-                    itemadd.IsVisible2 = 0;
-            break;
-            case "IceCream":
-                      itemadd.IsVisible3 = 0;
-              break;
-              case "Choco":
-                        itemadd.IsVisible4 = 0;
-                break;
-                case "Maggi":
-                          itemadd.IsVisible5 = 0;
-                  break;
-        default:
-
-      }
-
-if(c>=5){
-
-  itemadd.IsVisible6 = 1;
+    //},
+    //controller: NarrowItDownController,
+    //controllerAs: 'menu',
+    //bindToController:true
+  }
+  return ddo;
 }
 
+  NarrowItDownController.$inject = ['MenuSearchService'];
+  function NarrowItDownController(MenuSearchService) {
+
+    var menu = this;
+
+
+
+    menu.searchTerm = undefined;
+
+
+
+    menu.butt = function () {
+
+    var promise =  MenuSearchService.getMatchedMenuItems(menu.searchTerm);
+
+      promise.then(function (temp) {
+
+        menu.categories = temp;
+        console.log(menu.categories);
+            console.log(menu.categories[0].name);
+
+
+
+      })
+      .catch(function (error) {
+        console.log("Something is wrong");
+
+      });
 
     }
 
-  }
+    menu.remove1 = function (Index,pass) {
 
-  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
-  function AlreadyBoughtController(ShoppingListCheckOffService){
-var showList = this;
+      MenuSearchService.remove(Index,pass);
 
-
-
-showList.items = ShoppingListCheckOffService.getItems();
-
+    };
 
   }
 
-  function ShoppingListCheckOffService()
-  {
+  MenuSearchService.$inject = ['$http'];
+  function MenuSearchService($http) {
     var service = this;
 
-    var items = [];
-
-    service.addItem = function (itemName,quantity) {
-
-      var item = {
-        name: itemName,
-        quantity: quantity
 
 
-      };
-      items.push(item);
+    service.getMatchedMenuItems = function (abc) {
 
 
+      return $http({
+        method : 'GET',
+        url : ('https://davids-restaurant.herokuapp.com/menu_items.json')
+      }).then(function (response) {
 
-  };
-      service.getItems = function () {
-          return items;
-      };
+        var found = [];
+    var temp = [];
 
+    temp = response.data.menu_items;
+    // console.log(temp[i].name);
+    for(var i=0;i<=218;i++)
+  {
+
+    if (temp[i].name.toLowerCase().indexOf(abc)=== -1)
+     {
 
     }
+    else {
+      found.push(temp[i])
+    }
+
+    }
+    return found;
+});
+
+  };
+
+service.remove = function (index,prod) {
+  prod.splice(index,1);
+};
+
+  }
+
 
 })();
